c003be808c9777cd511894f1ec19d840bd8240fd
Marjan Gjoni <mgjoni@us.ibm.com>
2010-01-26 18:09:53 -0500

    Trimmed out useless functions.  Added stub for saving.  save is set to None if nothing is specified on the commandline.

--- genJvmArgs.py
+++ genJvmArgs.py
@@ -2,17 +2,14 @@ import sys
 argSearch=sys.argv[2]
 jvmSearch=sys.argv[1]
 operation=sys.argv[0]
-#newwpsargs=sys.argv[1]
 try:
   save=sys.argv[3]
 except:
-  save=''
-#jvms = {}
+  save=None
 
 getJvmArguments = lambda x: AdminConfig.show(x, ['genericJvmArguments']).split('genericJvmArguments ')[1][1:-2]
 
 jvmList = [AdminConfig.getid("/Server:%s/JavaProcessDef:/JavaVirtualMachine:/" % jvm.split('(')[0]) for jvm in AdminConfig.list('Server').splitlines() if jvm.find(jvmSearch)>=0 ]
-#map(lambda x: displayGJA(x), jvmList)
 
 def addArgs(newArg, jvm, save):
   node = jvm.split('nodes/')[1].split('/')[0]
@@ -23,26 +20,23 @@ def addArgs(newArg, jvm, save):
     sys.exit(1)
   else:
     new = "%s %s" % (argList, newArg)
-    AdminConfig.modify(id, [['genericJvmArguments', new]])
-
-def printCols(item1,item2):
-  firstCol = 10 - len(item1)
-  print "%(buffer)-2s %( )s %(gen)s" % {'buffer': item1, ' ': ' '*firstCol, 'gen':item2}
+    AdminConfig.modify(jvm, [['genericJvmArguments', new]])
+    if save:
+       print "save"
 
 def removeArgs(argSearch, jvm, save):
   argList = getJvmArguments(jvm)
   newArgList = ' '.join([arg for arg in argList.split(' ') if arg.find(argSearch) < 0])
   jvmDisplayName = jvm.split('servers/')[1].split('|')[0]
-  if (save != 'save'):
-    print jvmDisplayName
-    printCols("Current",argList)
-    printCols("New",newArgList)
-  else:
-    print jvmDisplayName
-    AdminConfig.modify(jvm, [['genericJvmArguments', newArgList]])
-    print getJvmArguments(jvm)
+  print jvmDisplayName
+  print "%s %s" % ('Current',argList)
+  AdminConfig.modify(jvm, [['genericJvmArguments', newArgList]])
+  print "%s %s" % ('New',getJvmArguments(jvm))
+  if save:
+    print "save"
 
-if operation == 'add':
-  map(lambda x: addArgs(argSearch, x, save), jvmList)
-elif operation == 'remove':
-  map(lambda x: removeArgs(argSearch, x, save), jvmList)
+if __name__ == "main":
+  if operation == 'add':
+    map(lambda x: addArgs(argSearch, x, save), jvmList)
+  elif operation == 'remove':
+    map(lambda x: removeArgs(argSearch, x, save), jvmList)
